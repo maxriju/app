3f12113a254cb4c28c91cbe82ac379bf8f5c7da1
maxrjtr <maxrjtr@gmail.com>
2024-04-15 16:21:44 -0600

    correcion de ererores

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
+const userRoutes = require('./src/routes/userRoutes');
+const errorHandler = require('./src/middleware/errorMiddleware');
 const express = require('express');
 const app = express();
+app.use(express.json());
 
-
-app.get('/api/saludo', (req, res) => {
+/*app.get('/api/saludo', (req, res) => {
     res.send('Hola, mundo!');
-  });
+  });*/
+
+app.use('/users', userRoutes);
+
+app.use(errorHandler);
+
 
 const PORT = process.env.PORT || 3000;
-console.log(process.env.env)
+
 
 
 app.listen(PORT, () => {
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,9 +1,9 @@
 const db = require('../config/dbConfig');
-const bcrypt = require('bcrypt');
+//const bcrypt = require('bcrypt');
 
 exports.getAllUsers = async (params = {}) => {
     return new Promise((resolve, reject) => {
-        let query = "SELECT * FROM users WHERE 1=1";
+        let query = "SELECT * FROM user WHERE 1=1";
         const values = [];
 
         if (params.status) {
@@ -12,17 +12,8 @@ exports.getAllUsers = async (params = {}) => {
         }
 
         if (params.name) {
-            query += " AND first_name LIKE ?";
+            query += " AND name LIKE ?";
             values.push(`%${params.name}%`);
-        }
-
-        if (params.page && params.pageSize) {
-            const pageSize = parseInt(params.pageSize, 10) || 10;
-            const page = parseInt(params.page, 10) || 1;
-            const offset = (page - 1) * pageSize;
-
-            query += " LIMIT ? OFFSET ?";
-            values.push(pageSize, offset);
         }
 
         db.query(query, values, (error, results) => {
@@ -30,6 +21,57 @@ exports.getAllUsers = async (params = {}) => {
                 reject(error);
             } else {
                 resolve(results);
+            }
+        });
+    });
+};
+
+
+exports.createUser = async (userData) => {
+    return new Promise((resolve, reject) => {
+        
+        if (!userData) {
+            reject("No user data will be provided.");
+            return;
+        }
+
+        const {name, last_name, position, user, password, status} = userData;
+
+        const sql = 'INSERT INTO user (name, last_name, position, user, password, status) VALUES (?, ?, ?, ?, ?, ?)';
+        db.query(sql, [name, last_name, position, user, password, status], (error, results) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(results.insertId);
+            }
+        });
+        
+    });
+};
+
+exports.updateUser = async (userId, userData) => {
+    return new Promise((resolve, reject) => {
+        const { name, last_name, position, user } = userData;
+
+        const sql = 'UPDATE user SET first_name=?, last_name=?, user=? WHERE user_id=?';
+        db.query(sql, [name, last_name, user, position, userId], (error, results) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(results.affectedRows > 0);
+            }
+        });
+    });
+};
+
+exports.deleteUser = async (userId) => {
+    return new Promise((resolve, reject) => {
+        const sql = 'DELETE FROM user WHERE user_id=?';
+        db.query(sql, [userId], (error, results) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(results.affectedRows > 0);
             }
         });
     });
