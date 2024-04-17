1d030755878c85a997ae2cbcfd7c8c1a54d0a2e7
maxrjtr <maxrjtr@gmail.com>
2024-04-17 08:33:21 -0600

    correcion de ererores en empleados

--- src/models/empleadosModel.js
+++ src/models/empleadosModel.js
@@ -35,10 +35,52 @@ exports.createEmpleado = async (empleadoData) => {
             return;
         }
 
-        const {estatus, clave, nombre_empleado, tipo_trabajador, estadisticos_auditorias, tipo_contrato, sexo, edad, edo_civil, tipo_paternidad, no_hijos, salario_hora, sd_inicial_alta, sd_actual, salario_semanal_bruto, puesto, nomina, departamento, jefe_directo, fecha_ingreso, estatus_recontratable, antigüedad, nivel_estudios_expediente, nivel_estudios_actualizacion, especialidad, habilidades_laborales, fecha_nacimiento, estado_nacimiento, rfc, curp, nss, calle_numero, colonia, municipio, estado, codigo_postal, telefono, email, cuenta_bancaria, ruta, turno_comida, observaciones} = empleadoData;
+        const {estatus,
+            clave,
+            nombre_empleado,
+            tipo_trabajador,
+            estadisticos_auditorias,
+            tipo_contrato,
+            sexo,
+            edad,
+            edo_civil,
+            tipo_paternidad,
+            no_hijos,
+            salario_hora,
+            sd_inicial_alta,
+            sd_actual,
+            salario_semanal_bruto,
+            puesto,
+            nomina,
+            departamento,
+            jefe_directo,
+            fecha_ingreso,
+            estatus_recontratable,
+            antiguedad,
+            nivel_estudios_expediente,
+            nivel_estudios_actualizacion,
+            especialidad,
+            habilidades_laborales,
+            fecha_nacimiento,
+            estado_nacimiento,
+            rfc,
+            curp,
+            nss,
+            calle_numero,
+            colonia,
+            municipio,
+            estado,
+            codigo_postal,
+            telefono,
+            email,
+            cuenta_bancaria,
+            ruta,
+            turno_comida,
+            observaciones} = empleadoData;
 
-        const sql = 'INSERT INTO empleados (estatus, clave, nombre_empleado, tipo_trabajador, estadisticos_auditorias, tipo_contrato, sexo, edad, edo_civil, tipo_paternidad, no_hijos, salario_hora, sd_inicial_alta, sd_actual, salario_semanal_bruto, puesto, nomina, departamento, jefe_directo, fecha_ingreso, estatus_recontratable, antigüedad, nivel_estudios_expediente, nivel_estudios_actualizacion, especialidad, habilidades_laborales, fecha_nacimiento, estado_nacimiento, rfc, curp, nss, calle_numero, colonia, municipio, estado, codigo_postal, telefono, email, cuenta_bancaria, ruta, turno_comida, observaciones) VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
-        db.query(sql, [estatus, clave, nombre_empleado, tipo_trabajador, estadisticos_auditorias, tipo_contrato, sexo, edad, edo_civil, tipo_paternidad, no_hijos, salario_hora, sd_inicial_alta, sd_actual, salario_semanal_bruto, puesto, nomina, departamento, jefe_directo, fecha_ingreso, estatus_recontratable, antigüedad, nivel_estudios_expediente, nivel_estudios_actualizacion, especialidad, habilidades_laborales, fecha_nacimiento, estado_nacimiento, rfc, curp, nss, calle_numero, colonia, municipio, estado, codigo_postal, telefono, email, cuenta_bancaria, ruta, turno_comida, observaciones], (error, results) => {
+        const sql = 'INSERT INTO empleados (estatus, clave, nombre_empleado, tipo_trabajador, estadisticos_auditorias, tipo_contrato, sexo, edad, edo_civil, tipo_paternidad, no_hijos, salario_hora, sd_inicial_alta, sd_actual, salario_semanal_bruto, puesto, nomina, departamento, jefe_directo, fecha_ingreso, estatus_recontratable, antiguedad, nivel_estudios_expediente, nivel_estudios_actualizacion, especialidad, habilidades_laborales, fecha_nacimiento, estado_nacimiento, rfc, curp, nss, calle_numero, colonia, municipio, estado, codigo_postal, telefono, email, cuenta_bancaria, ruta, turno_comida, observaciones) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
+        db.query(sql, [estatus, clave, nombre_empleado, tipo_trabajador, estadisticos_auditorias, tipo_contrato, sexo, edad, edo_civil, tipo_paternidad, no_hijos, salario_hora, sd_inicial_alta, sd_actual, salario_semanal_bruto, puesto, nomina, departamento, jefe_directo, fecha_ingreso, estatus_recontratable, antiguedad, nivel_estudios_expediente, nivel_estudios_actualizacion, especialidad, habilidades_laborales, fecha_nacimiento, estado_nacimiento, rfc, curp, nss, calle_numero, colonia, municipio, estado, codigo_postal, telefono, email, cuenta_bancaria, ruta, turno_comida, observaciones
+        ], (error, results) => {
             if (error) {
                 reject(error);
             } else {
@@ -51,10 +93,10 @@ exports.createEmpleado = async (empleadoData) => {
 
 exports.updateEmpleado = async (empleadoId, empleadoData) => {
     return new Promise((resolve, reject) => {
-        const { estatus, clave, nombre_empleado, tipo_trabajador, estadisticos_auditorias, tipo_contrato, sexo, edad, edo_civil, tipo_paternidad, no_hijos, salario_hora, sd_inicial_alta, sd_actual, salario_semanal_bruto, puesto, nomina, departamento, jefe_directo, fecha_ingreso, estatus_recontratable, antigüedad, nivel_estudios_expediente, nivel_estudios_actualizacion, especialidad, habilidades_laborales, fecha_nacimiento, estado_nacimiento, rfc, curp, nss, calle_numero, colonia, municipio, estado, codigo_postal, telefono, email, cuenta_bancaria, ruta, turno_comida, observaciones } = empleadoData;
+        const { estatus, clave, nombre_empleado, tipo_trabajador, estadisticos_auditorias, tipo_contrato, sexo, edad, edo_civil, tipo_paternidad, no_hijos, salario_hora, sd_inicial_alta, sd_actual, salario_semanal_bruto, puesto, nomina, departamento, jefe_directo, fecha_ingreso, estatus_recontratable, antiguedad, nivel_estudios_expediente, nivel_estudios_actualizacion, especialidad, habilidades_laborales, fecha_nacimiento, estado_nacimiento, rfc, curp, nss, calle_numero, colonia, municipio, estado, codigo_postal, telefono, email, cuenta_bancaria, ruta, turno_comida, observaciones } = empleadoData;
 
-        const sql = "UPDATE empleados SET estatus=?, clave=?, nombre_empleado=?, tipo_trabajador=?, estadisticos_auditorias=?, tipo_contrato=?, sexo=?, edad=?, edo_civil=?, tipo_paternidad=?, no_hijos=?, salario_hora=?, sd_inicial_alta=?, sd_actual=?, salario_semanal_bruto=?, puesto=?, nomina=?, departamento=?, jefe_directo=?, fecha_ingreso=?, estatus_recontratable=?, antigüedad=?, nivel_estudios_expediente=?, nivel_estudios_actualizacion=?, especialidad=?, habilidades_laborales=?, fecha_nacimiento=?, estado_nacimiento=?, rfc=?, curp=?, nss=?, calle_numero=?, colonia=?, municipio=?, estado=?, codigo_postal=?, telefono=?, email=?, cuenta_bancaria=?, ruta=?, turno_comida=?, observaciones=? WHERE id=?";
-        db.query(sql, [estatus, clave, nombre_empleado, tipo_trabajador, estadisticos_auditorias, tipo_contrato, sexo, edad, edo_civil, tipo_paternidad, no_hijos, salario_hora, sd_inicial_alta, sd_actual, salario_semanal_bruto, puesto, nomina, departamento, jefe_directo, fecha_ingreso, estatus_recontratable, antigüedad, nivel_estudios_expediente, nivel_estudios_actualizacion, especialidad, habilidades_laborales, fecha_nacimiento, estado_nacimiento, rfc, curp, nss, calle_numero, colonia, municipio, estado, codigo_postal, telefono, email, cuenta_bancaria, ruta, turno_comida, observaciones, empleadoId], (error, results) => {
+        const sql = "UPDATE empleados SET estatus=?, clave=?, nombre_empleado=?, tipo_trabajador=?, estadisticos_auditorias=?, tipo_contrato=?, sexo=?, edad=?, edo_civil=?, tipo_paternidad=?, no_hijos=?, salario_hora=?, sd_inicial_alta=?, sd_actual=?, salario_semanal_bruto=?, puesto=?, nomina=?, departamento=?, jefe_directo=?, fecha_ingreso=?, estatus_recontratable=?, antiguedad=?, nivel_estudios_expediente=?, nivel_estudios_actualizacion=?, especialidad=?, habilidades_laborales=?, fecha_nacimiento=?, estado_nacimiento=?, rfc=?, curp=?, nss=?, calle_numero=?, colonia=?, municipio=?, estado=?, codigo_postal=?, telefono=?, email=?, cuenta_bancaria=?, ruta=?, turno_comida=?, observaciones=? WHERE id=?";
+        db.query(sql, [estatus, clave, nombre_empleado, tipo_trabajador, estadisticos_auditorias, tipo_contrato, sexo, edad, edo_civil, tipo_paternidad, no_hijos, salario_hora, sd_inicial_alta, sd_actual, salario_semanal_bruto, puesto, nomina, departamento, jefe_directo, fecha_ingreso, estatus_recontratable, antiguedad, nivel_estudios_expediente, nivel_estudios_actualizacion, especialidad, habilidades_laborales, fecha_nacimiento, estado_nacimiento, rfc, curp, nss, calle_numero, colonia, municipio, estado, codigo_postal, telefono, email, cuenta_bancaria, ruta, turno_comida, observaciones, empleadoId], (error, results) => {
             if (error) {
                 reject(error);
             } else {
