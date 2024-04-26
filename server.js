2aee515706805332f078141b5ecd74f5c14a2d62
maxrjtr <maxrjtr@gmail.com>
2024-04-26 13:25:49 -0600

    Correciones pocas

--- server.js
+++ server.js
@@ -8,19 +8,33 @@ const altaBajaEmpleadoRoute = require('./src/routes/altasYBajasRoutes');
 const errorHandler = require('./src/middleware/errorMiddleware');
 
 const express = require('express');
+//const cors = require('cors');
+
 const app = express();
+//app.use(cors());
 app.use(express.json());
+
+/*
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+*/
 
 app.get('/api/saludo', (req, res) => {
     res.json({ message: "Hola desde el servidor!" });
   });
 
-app.use('/users', userRoutes);
-app.use('/empleados', empleadoRoute);
-app.use('/hijos-empleado', hijosEmpladoRuote);
-app.use('/formacion-empleados', formacionRoute);
-app.use('/experienca-empleado', experienciaRoute),
-app.use('/altas-bajas-empleado', altaBajaEmpleadoRoute);
+
+app.use('/api/users', userRoutes);
+app.use('/api/empleados', empleadoRoute);
+app.use('/api/hijos-empleado', hijosEmpladoRuote);
+app.use('/api/formacion-empleados', formacionRoute);
+app.use('/api/experienca-empleado', experienciaRoute);
+app.use('/api/altas-bajas-empleado', altaBajaEmpleadoRoute);
 
 app.use(errorHandler);
 
